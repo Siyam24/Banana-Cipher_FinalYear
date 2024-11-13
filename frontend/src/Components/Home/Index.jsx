d9f8e1c90327897d0437cc715a1c0bb37d7262a0
Siyam Vijayaraj <vijayarajsiyam01@gmail.com>
2024-11-13 20:27:27 +0530

    Home page

--- frontend/src/Components/Home/Index.jsx
+++ frontend/src/Components/Home/Index.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from './homeStyles.module.css';
 
 const Home = () => {
   return (
     <div>
-      <h1>Banana Cipher🍌</h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.homeButton}>New Game</button>
+        <button className={styles.homeButton}>Score!</button>
+        <button className={styles.homeButton}>Exit</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
